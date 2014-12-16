9e074d5ff0b4a5ddd47b8663bb437e153122d330
AmelHalilovic <ahalilovi10@etf.unsa.ba>
2014-12-16 22:56:33 +0100

    design

--- DBCResort/Hotel2/Models/DBCResort.Designer.cs
+++ DBCResort/Hotel2/Models/DBCResort.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\NerA\Desktop\DBC-Resort-master\Hotel2\Models\DBCResort.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Amel\Desktop\DBCResort\DBCResort\Hotel2\Models\DBCResort.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
