1190536795f5d3243be6aa66cc1ffa8cdb0eac48
AmelHalilovic <ahalilovi10@etf.unsa.ba>
2014-12-16 19:56:48 +0100

    SessionController added, login and registration form

---
+++ DBCResort/Hotel2/Controllers/SessionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Hotel2.Controllers
+{
+    public class SessionController : Controller
+    {
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public void Create(string username, string password)
+        {
+            System.Web.HttpContext.Current.Response.Write("Test create:" + username + password);
+            //Response.Redirect("http://localhost:50674");
+
+        }
+
+
+    }
+}
